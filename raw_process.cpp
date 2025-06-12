6a1e12f231e0a2b3e1713207e83a8eab15b6a3f0
Hank Wang <kfaswhy@gmail.com>
2025-06-12 10:14:14 +0800

    v2.2.3 删除不必要的函数

--- raw_process.cpp
+++ raw_process.cpp
@@ -231,7 +231,6 @@ RGB* raw2rgb(U16* raw, IMG_CONTEXT context, G_CONFIG cfg) {
 }
 
 
-
 U16* readraw(const char* filename, IMG_CONTEXT context, G_CONFIG cfg)
 {
     int bytesPerPixel = (cfg.bit + 7) / 8;  // 计算每像素的字节数
@@ -271,30 +270,6 @@ U16* readraw(const char* filename, IMG_CONTEXT context, G_CONFIG cfg)
     return raw;
 }
 
-RGB* yyy2rgb_process(YUV* yuv, IMG_CONTEXT context, G_CONFIG cfg)
-{
-	if (!yuv) {
-		std::cerr << "Invalid input parameters!" << std::endl;
-		return NULL; // 错误代码
-	}
-
-	RGB* rgb = (RGB*)calloc(context.height * context.width, sizeof(RGB));
-
-	RGB* tmp = rgb;
-	for (U32 i = 0; i < context.full_size; ++i) 
-	{
-		yuv->y[i] = clp_range(0, yuv->y[i], 255);
-		tmp->r = yuv->y[i];
-		tmp->g = yuv->y[i];
-		tmp->b = yuv->y[i];
-		tmp++;
-	}
-	LOG("done.");
-
-	return rgb;
-}
-
-
 void safe_free(void* p)
 {
 	if (NULL != p)
@@ -313,93 +288,6 @@ void print_prog(U32 cur_pos, U32 tgt)
 		LOG("Processing: %d%%.", cur_pos * 100 / tgt);
 		time_print_prog_start = clock();
 	}
-	return;
-}
-
-RGB* load_bmp(const char* filename, IMG_CONTEXT* context)
-{
-	FILE* f_in = fopen(filename, "rb");
-	if (f_in == NULL)
-	{
-		LOG("Cannot find %s", filename);
-		return NULL;
-	}
-
-	fread(&context->fileHeader, sizeof(BITMAPFILEHEADER), 1, f_in);
-	fread(&context->infoHeader, sizeof(BITMAPINFOHEADER), 1, f_in);
-
-	context->height = context->infoHeader.biHeight;
-	context->width = context->infoHeader.biWidth;
-
-	//context->w_samp = context->width / cfg.sample_ratio;
-	//context->h_samp = context->height / cfg.sample_ratio;
-
-
-	int LineByteCnt = (((context->width * context->infoHeader.biBitCount) + 31) >> 5) << 2;
-	//int ImageDataSize = LineByteCnt * height;
-	context->PaddingSize = 4 - ((context->width * context->infoHeader.biBitCount) >> 3) & 3;
-	context->pad = (BYTE*)malloc(sizeof(BYTE) * context->PaddingSize);
-	RGB* img = (RGB*)malloc(sizeof(RGB) * context->height * context->width);
-
-	if (context->infoHeader.biBitCount == 24) {
-		for (int i = 0; i < context->height; i++) {
-			for (int j = 0; j < context->width; j++) {
-				int index = i * context->width + j;
-				fread(&img[index], sizeof(RGB), 1, f_in);
-			}
-			if (context->PaddingSize != 0)
-			{
-				fread(context->pad, 1, context->PaddingSize, f_in);
-			}
-		}
-	}
-	else
-	{
-		LOG("Only support BMP in 24-bit.");
-		return NULL;
-	}
-
-	fclose(f_in);
-	return img;
-}
-
-void save_bmp(const char* filename, RGB* img, IMG_CONTEXT* context)
-{
-	FILE* f_out = fopen(filename, "wb");
-
-	context->fileHeader.bfType = 0x4D42; // 'BM'
-	context->fileHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-	context->fileHeader.bfSize = context->fileHeader.bfOffBits + context->width * context->height * sizeof(RGB);
-	context->fileHeader.bfReserved1 = 0;
-	context->fileHeader.bfReserved2 = 0;
-
-	context->infoHeader.biSize = sizeof(BITMAPINFOHEADER);
-	context->infoHeader.biWidth = context->width;
-	context->infoHeader.biHeight = context->height;
-	context->infoHeader.biPlanes = 1;
-	context->infoHeader.biBitCount = 24;
-	context->infoHeader.biCompression = 0;
-	context->infoHeader.biSizeImage = context->width * context->height * sizeof(RGB);
-	context->infoHeader.biXPelsPerMeter = 0;
-	context->infoHeader.biYPelsPerMeter = 0;
-	context->infoHeader.biClrUsed = 0;
-	context->infoHeader.biClrImportant = 0;
-
-	fwrite(&context->fileHeader, sizeof(context->fileHeader), 1, f_out);
-	fwrite(&context->infoHeader, sizeof(context->infoHeader), 1, f_out);
-
-	for (int i = 0; i < context->height; i++)
-	{
-		for (int j = 0; j < context->width; j++)
-		{
-			fwrite(&img[i * context->width + j], sizeof(RGB), 1, f_out);
-		}
-		if (context->PaddingSize != 0)
-		{
-			fwrite(context->pad, 1, context->PaddingSize, f_out);
-		}
-	}
-	fclose(f_out);
 	return;
 }
 
@@ -480,7 +368,6 @@ void save_y(const char* filename, U16* y, U16 width, U16 height, U8 bit, int com
 		fprintf(stderr, "Failed to write image: %s\n", filename);
 	}
 }
-
 
 void save_img(const char* filename, RGB* img, U16 w, U16 h, U8 bit, int compression_quality = 100)
 {
@@ -597,8 +484,6 @@ U16* gauss_filter(U16* y, U16 height, U16 width, U8 r)
 	free(kernel);
 	return out;
 }
-
-
 
 static int compare_u16(const void* a, const void* b) {
 	U16 va = *(const U16*)a;
