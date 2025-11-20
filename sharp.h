48c5a0a9ae413c9e05967fa084deea7cd8dac92b
Hank <kfaswhy@gmail.com>
2025-11-20 16:05:42 +0800

    v2.7 sharpdev

--- sharp.h
+++ sharp.h
@@ -4,5 +4,7 @@
 
 U8 sharp_process(YUV* yuv, IMG_CONTEXT context, G_CONFIG cfg);
 
+S32* calc_edge_em_3x5(U16* y, int width, int height);
+
 void edge_detect(U16* src, S32* dst, U16 height, U16 width, S8* kernel, U8 k_size);
 
